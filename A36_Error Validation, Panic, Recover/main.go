128897a99b29bf1012c962a0759bf160192c78ab
Aar <armannugraha85@gmail.com>
2020-09-20 11:19:37 +0700

    update code a36.1

--- A36_Error Validation, Panic, Recover/main.go
+++ A36_Error Validation, Panic, Recover/main.go
@@ -1,24 +1,45 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"strconv"
+	"strings"
 )
 
+func validate(input string) (bool, error) {
+	if strings.TrimSpace(input) == "" {
+		return false, errors.New("cannot be empty")
+	}
+
+	return true, nil
+}
+
 func main() {
-	var input string
+	// A.36.1
+	// var input string
 
-	fmt.Println("Type some number : ")
-	fmt.Scanln(&input)
+	// fmt.Println("Type some number : ")
+	// fmt.Scanln(&input)
 
-	var number int
-	var err error
-	number, err = strconv.Atoi(input)
+	// var number int
+	// var err error
+	// number, err = strconv.Atoi(input)
 
-	if err == nil {
-		fmt.Println(number, " Is number")
+	// if err == nil {
+	// 	fmt.Println(number, " Is number")
+	// } else {
+	// 	fmt.Println(input, " Is not number")
+	// 	fmt.Println(err.Error())
+	// }
+
+	// A.36.2
+	var name string
+	fmt.Print("Type Your Name :")
+	fmt.Scanln(&name)
+
+	if valid, err := validate(name); valid {
+		fmt.Println("Hallo", name)
 	} else {
-		fmt.Println(input, " Is not number")
 		fmt.Println(err.Error())
 	}
 
